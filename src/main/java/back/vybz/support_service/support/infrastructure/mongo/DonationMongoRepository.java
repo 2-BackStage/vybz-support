cea676349be4bede268f0d8984995fa12846fafb
jijihorang <jiho7213@gmail.com>
2025-06-19 11:34:19 +0900

    refactor : comment 수정

--- src/main/java/back/vybz/support_service/support/infrastructure/mongo/DonationMongoRepository.java
+++ src/main/java/back/vybz/support_service/support/infrastructure/mongo/DonationMongoRepository.java
@@ -4,12 +4,7 @@ import back.vybz.support_service.support.domain.mongo.Donation;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
-import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
-@Repository
 public interface DonationMongoRepository extends MongoRepository<Donation, String> {
 
     boolean existsByUserUuid(String userUuid);
